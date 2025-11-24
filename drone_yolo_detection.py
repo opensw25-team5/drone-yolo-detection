76ef154ce8fce11e685a07831cd3c9085641cc48
Kinetic27 <aheui@kakao.com>
2025-11-24 20:06:00 +0900

    Feat Add keyboard flight control (takeoff, land, move)

--- drone_yolo_detection.py
+++ drone_yolo_detection.py
@@ -21,6 +21,7 @@ class TelloYOLOController:
         self.frame_reader = self.drone.get_frame_read()
         self.frame_width, self.frame_height = 960, 720
 
+        self.has_taken_off = False
         self.running = True
 
         # 스레드 통신용 큐 생성
@@ -32,7 +33,11 @@ class TelloYOLOController:
         self.detection_thread = threading.Thread(
             target=self.process_detections, daemon=True
         )
+        self.control_thread = threading.Thread(
+            target=self.execute_commands, daemon=True
+        )
 
+        self.control_thread.start()
         self.capture_thread.start()
         self.detection_thread.start()
 
@@ -65,6 +70,32 @@ class TelloYOLOController:
                         pass
                 self.detection_queue.put(annotated_frame)
             sleep(0.005)
+
+    def execute_commands(self):
+        while self.running:
+            cmd = input("Command (t:takeoff, l:land, w/s/a/d:move): ")
+            if cmd == "t" and not self.has_taken_off:
+                self.drone.takeoff()
+                self.has_taken_off = True
+            elif cmd == "l" and self.has_taken_off:
+                self.drone.land()
+                self.has_taken_off = False
+            elif cmd == "w":
+                self.drone.move_forward(30)
+            elif cmd == "s":
+                self.drone.move_back(30)
+            elif cmd == "a":
+                self.drone.move_left(30)
+            elif cmd == "d":
+                self.drone.move_right(30)
+            elif cmd == "i":
+                self.drone.move_up(30)
+            elif cmd == "o":
+                self.drone.move_down(30)
+            elif cmd == "q":
+                self.running = False
+                break
+            sleep(0.1)
 
     def run(self):
         try:
